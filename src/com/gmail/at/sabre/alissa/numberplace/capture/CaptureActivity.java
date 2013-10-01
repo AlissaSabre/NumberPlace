d76ca33e8745f1752ef12b23c1f486318e28723a
alissa <alissa_sabre@yahoo.co.jp>
2013-10-01 19:26:27 +0900

    Added a mock Capture activity.

---
+++ src/com/gmail/at/sabre/alissa/numberplace/capture/CaptureActivity.java
@@ -0,0 +1,15 @@
+package com.gmail.at.sabre.alissa.numberplace.capture;
+
+import com.gmail.at.sabre.alissa.numberplace.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CaptureActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture);
+    }
+}
