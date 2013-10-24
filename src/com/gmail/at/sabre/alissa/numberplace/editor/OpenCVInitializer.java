63baadbeafd9c300b2eca649c3c00c631d074220
alissa <alissa_sabre@yahoo.co.jp>
2013-10-24 22:02:42 +0900

    Visits OpenCV Manager download page as a new task. The download URI is taken from resource.
    A lot of comments to OpenCV Initializer

--- src/com/gmail/at/sabre/alissa/numberplace/editor/OpenCVInitializer.java
+++ src/com/gmail/at/sabre/alissa/numberplace/editor/OpenCVInitializer.java
@@ -17,6 +17,8 @@ import com.gmail.at.sabre.alissa.numberplace.R;
 
 /***
  * A wrapper around {@link OpenCVLoader} including its own implementation of {@link LoaderCallbackInterface}.
+ * This class is intended to be general use,
+ * though the targeted use cases are of Number Place Breaker app.
  *
  * @author alissa
  */
@@ -48,7 +50,10 @@ public class OpenCVInitializer {
 
 	/***
 	 * Initialize OpenCV library.
-	 * This method returns soon, and one of the specified actions will be run later.
+	 * This method may be called by any thread.
+	 * It returns immediately,
+	 * and one of the three specified actions will be run by the UI thread later,
+	 * when the initialization is finished.
 	 * One, two, or all three actions may be null.
 	 * <p>
 	 * If OpenCV Manager is installed on the device,
@@ -63,7 +68,7 @@ public class OpenCVInitializer {
 	 * it runs {@code pending} action before the user completes installation.
 	 * <p>
 	 * The {@code pending} action may be run under other circumstances,
-	 * e.g., the user dismissed the prompt, by pressing a BACK button,
+	 * e.g., the user dismissed the prompt by pressing a BACK button,
 	 * not answering the question,
 	 * or the process was interrupted by some temporary failure such as
 	 * low system resources.
@@ -113,6 +118,9 @@ public class OpenCVInitializer {
 
 	/***
 	 * An instance of {@link Runnable} that does nothing when run.
+	 * This is only used in {@link Callback},
+	 * so it should have been private in the class,
+	 * but Java doesn't allow it.
 	 */
 	private static final Runnable EMPTY_RUNNABLE = new Runnable() {
 		public void run() {}
@@ -131,18 +139,41 @@ public class OpenCVInitializer {
 
 		private final Runnable mPending;
 
+		/***
+		 * The constructor.
+		 * It receives three {@link Runnable}s from
+		 * {@link OpenCVInitializer#initialize(Runnable, Runnable, Runnable)}.
+		 * They can be null, and null actions are substituted by {@link OpenCVInitializer#EMPTY_RUNNABLE}.
+		 */
 		public Callback(final Runnable success, final Runnable failure, final Runnable pending) {
 			mSuccess = success == null ? EMPTY_RUNNABLE : success;
 			mFailure = failure == null ? EMPTY_RUNNABLE : failure;
 			mPending = pending == null ? EMPTY_RUNNABLE : pending;
 		}
 
+		/***
+		 * This method is called back when {@link OpenCVLoader} wants to
+		 * install OpenCV Manager (as well as something else in a future?)
+		 * The expected action is to show a dialog and get an approval to
+		 * do so from the user.
+		 * This implementation, however, never shows a dialog,
+		 * and always cancels the request.
+		 * We will show our dialog later, in {@link #onManagerConnected(int)}.
+		 * @param operation
+		 *            The requested operation. This implementation doesn't use it.
+		 * @param callback
+		 *            A callback that this method tells to {@link OpenCVLoader}
+		 *            the user's decision.
+		 *            This implementation always calls {@link InstallCallbackInterface#cancel()} immediately.
+		 */
 		public void onPackageInstall(final int operation, final InstallCallbackInterface callback) {
-			// We always cancel the installation with no interaction with the user.
-			// Installation guidance will be given in the code to handle INSTALL_CANCELED below.
 			callback.cancel();
 		}
 
+		/***
+		 * This method is called back when {@link OpenCVLoader} finished
+		 * the initialization, regardless it was successful.
+		 */
 		public void onManagerConnected(final int status) {
 			switch (status) {
 
@@ -154,7 +185,7 @@ public class OpenCVInitializer {
 				return;
 
 			case INSTALL_CANCELED:
-				// Since we always invoke InstallCallbackInterface.cancel in onPackageInstall,
+				// Since we always invoke InstallCallbackInterface.cancel() in onPackageInstall,
 				// we always come here if OpenCV Manager is not installed.
 				/* fall through */
 
@@ -165,12 +196,18 @@ public class OpenCVInitializer {
 				// I'm not sure it is ever possible,
 				// because the the latest version (i.e., 2.4.6r2) support the oldest
 				// released version of OpenCV Manager.
-				// I believe it means as long as we stick on the library we have now,
-				// the INCOMPATIBLE_MANAGER_VERSION never occurs.
+				// I believe the INCOMPATIBLE_MANAGER_VERSION never occurs
+				// as long as we stick on the library we have now.
+				// (Unless the OpenCV developers released in a future older versions
+				// than the very first release...)
+				//
 				// Moreover, although the interface explicitly defines this case,
 				// it lacks the opposite case; the installed OpenCV is too new.
-				// I'm afraid it surely happens in a future.
+				// I'm afraid it can happen in a future, and the current API has
+				// no way to tell the situation to this app.
+				//
 				// Hmmmm.
+				//
 				// Anyway, we will guide the user to the Google Play
 				// for downloading another version of the OpenCV Manager.
 				suggestInstallation();
@@ -181,13 +218,15 @@ public class OpenCVInitializer {
 				// Because our onPackageInstall never calls InstallCallbackInterface.install(),
 				// this status should not occur.
 				/* fall through */
+
 			case INIT_FAILED:
 				// OpenCVLoader.initAsync failed with some unexpected reason.
 				// Users have nothing to do to solve the case.
 				// Just show a generic failure message.
 				/* fall through */
+
 			default:
-				// There are no other statuses.
+				// There are no other defined statuses.
 				// If we received other status value than the four above,
 				// it should be a bug of OpenCVLoader.
 				reportFailure();
@@ -195,6 +234,16 @@ public class OpenCVInitializer {
 			}
 		}
 
+		/***
+		 * Show a dialog to tell initialization failure.
+		 * The message (text content) is taken from the hosting activity's resource
+		 * using id {@link R.string#opencv_failure},
+		 * as well as the label for the only button using {@link R.string#opencv_ok}.
+		 * It should use general wording; details are not notified to the user.
+		 * <p>
+		 * After the user acknowledged or dismissed the dialog,
+		 * the callback's failure action ({@link #mFailure}) will be run.
+		 */
 		private void reportFailure() {
 			mHandler.post(new Runnable() {
 				public void run() {
@@ -214,6 +263,22 @@ public class OpenCVInitializer {
 			});
 		}
 
+		/***
+		 * Show a dialog to suggest installing OpenCV Manager.
+		 * The title, message (text contents), and button labels are taken
+		 * from the hosting activity's resource using the following ids:
+		 * {@link R.string#opencv_title} for the title,
+		 * {@link R.string#opencv_install} for the message,
+		 * {@link R.string#opencv_cancel} for the positive button label,
+		 * and {@link R.string#opencv_cancel} for the negative button label.
+		 * <p>
+		 * If the user acknowledged positively,
+		 * the download page of OpenCV Manager will be visited.
+		 * <p>
+		 * After the user acknowledged the dialog,
+		 * an appropriate action of this callback's three actions
+		 * (i.e., {@link #mSuccess}, {@link #mFailure}, or {@link #mPending} will be run.
+		 */
 		private void suggestInstallation() {
 			mHandler.post(new Runnable() {
 				private boolean mActionPosted = false;
@@ -250,11 +315,17 @@ public class OpenCVInitializer {
 			});
 		}
 
+		/***
+		 * Start an activity to show the download page of OpenCV Manager on Google Play App Store.
+		 * The actual URI to visit is taken from the hosting activity's resource
+		 * using an id {@link R.string#opencv_uri}.
+		 */
 		private void goForDownloading() {
 			// I believe https: scheme is better than market: scheme,
 			// because the user usually has more control over started activity,
-			final Uri download = Uri.parse("https://play.google.com/store/apps/details?id=org.opencv.engine");
+			final Uri download = Uri.parse(mContext.getString(R.string.opencv_uri));
 			Intent intent = new Intent(Intent.ACTION_VIEW, download);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 			mContext.startActivity(intent);
 		}
 	}
