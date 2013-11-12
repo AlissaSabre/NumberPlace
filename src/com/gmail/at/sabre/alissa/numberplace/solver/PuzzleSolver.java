02fef50edbde1d7e12ad409bbdac16c625017b28
alissa <alissa_sabre@yahoo.co.jp>
2013-11-12 22:27:01 +0900

    The previous commit that replaced hard-coded constants with symbolic constants in PuzzleSolver was incomplete.
    I considered that NN is a better name than M for a constant
    to mean N*N.

--- src/com/gmail/at/sabre/alissa/numberplace/solver/PuzzleSolver.java
+++ src/com/gmail/at/sabre/alissa/numberplace/solver/PuzzleSolver.java
@@ -24,7 +24,7 @@ public class PuzzleSolver {
 	 * The dimension (i.e., both the height and the width) of the whole puzzle.
 	 * It is 9 for a standard Number Place puzzle.
 	 */
-	private static final int M = N * N;
+	private static final int NN = N * N;
 
     private volatile boolean mIsStopped;
 
@@ -105,10 +105,10 @@ public class PuzzleSolver {
     private void addPuzzleRules() throws ContradictionException {
 
         // Each cell contains a digit.
-        for (int y = 0; y < M; y++) {
-            for (int x = 0; x < M; x++) {
-                final VecInt v = new VecInt(M);
-                for (int d = 1; d <= M; d++) {
+        for (int y = 0; y < NN; y++) {
+            for (int x = 0; x < NN; x++) {
+                final VecInt v = new VecInt(NN);
+                for (int d = 1; d <= NN; d++) {
                     v.push(var(x, y, d));
                 }
                 mSolver.addClause(v);
@@ -117,10 +117,10 @@ public class PuzzleSolver {
 
         // No two digits share a same cell.
         // (Each cell contains only one digit.)
-        for (int d1 = 1; d1 <= M; d1++) {
-            for (int d2 = d1 + 1; d2 <= M; d2++) {
-                for (int y = 0; y < M; y++) {
-                    for (int x = 0; x < M; x++) {
+        for (int d1 = 1; d1 <= NN; d1++) {
+            for (int d2 = d1 + 1; d2 <= NN; d2++) {
+                for (int y = 0; y < NN; y++) {
+                    for (int x = 0; x < NN; x++) {
                         final VecInt v = new VecInt(2);
                         v.push(-var(x, y, d1));
                         v.push(-var(x, y, d2));
@@ -131,10 +131,10 @@ public class PuzzleSolver {
         }
 
         // No two cells in a row contain a same digit.
-        for (int y = 0; y < M; y++) {
-            for (int x1 = 0; x1 < M; x1++) {
-                for (int x2 = x1 + 1; x2 < M; x2++) {
-                    for (int d = 1; d <= M; d++) {
+        for (int y = 0; y < NN; y++) {
+            for (int x1 = 0; x1 < NN; x1++) {
+                for (int x2 = x1 + 1; x2 < NN; x2++) {
+                    for (int d = 1; d <= NN; d++) {
                         final VecInt v = new VecInt(2);
                         v.push(-var(x1, y, d));
                         v.push(-var(x2, y, d));
@@ -145,10 +145,10 @@ public class PuzzleSolver {
         }
 
         // No two cells in a column contain a same digit.
-        for (int x = 0; x < M; x++) {
-            for (int y1 = 0; y1 < M; y1++) {
-                for (int y2 = y1 + 1; y2 < M; y2++) {
-                    for (int d = 1; d <= M; d++) {
+        for (int x = 0; x < NN; x++) {
+            for (int y1 = 0; y1 < NN; y1++) {
+                for (int y2 = y1 + 1; y2 < NN; y2++) {
+                    for (int d = 1; d <= NN; d++) {
                         final VecInt v = new VecInt(2);
                         v.push(-var(x, y1, d));
                         v.push(-var(x, y2, d));
@@ -159,15 +159,15 @@ public class PuzzleSolver {
         }
 
         // No two cells in a 3x3 block contain a same digit.
-        for (int y0 = 0; y0 < M; y0 += N) {
-            for (int x0 = 0; x0 < M; x0 += N) {
-                for (int p1 = 0; p1 < M; p1++) {
+        for (int y0 = 0; y0 < NN; y0 += N) {
+            for (int x0 = 0; x0 < NN; x0 += N) {
+                for (int p1 = 0; p1 < NN; p1++) {
                     final int x1 = x0 + p1 % N;
                     final int y1 = y0 + p1 / N;
-                    for (int p2 = p1 + 1; p2 < M; p2++) {
+                    for (int p2 = p1 + 1; p2 < NN; p2++) {
                         final int x2 = x0 + p2 % N;
                         final int y2 = y0 + p2 / N;
-                        for (int d = 1; d <= M; d++) {
+                        for (int d = 1; d <= NN; d++) {
                             final VecInt v = new VecInt(2);
                             v.push(-var(x1, y1, d));
                             v.push(-var(x2, y2, d));
@@ -189,8 +189,8 @@ public class PuzzleSolver {
      *             When the puzzle included a trivial contradiction.
      */
     private void addInstanceConstraints(final byte[][] puzzle) throws ContradictionException {
-        for (int y = 0; y < M; y++) {
-            for (int x = 0; x < M; x++) {
+        for (int y = 0; y < NN; y++) {
+            for (int x = 0; x < NN; x++) {
                 final int d = puzzle[y][x];
                 if (d > 0) {
                     final VecInt v = new VecInt(1);
@@ -213,7 +213,7 @@ public class PuzzleSolver {
      */
     private byte[][] extractSolution() {
         int[] model = mSolver.model();
-        byte[][] solution = new byte[9][9];
+        byte[][] solution = new byte[NN][NN];
 
         for (int i = 0; i < model.length; i++) {
             final int id = model[i];
@@ -233,7 +233,7 @@ public class PuzzleSolver {
     /***
      * The number of SAT4J variables we need to represent a number place puzzle.
      */
-    private static final int MAXVAR = 9 * 9 * 9 + 1; // XXX: Do we really need '+1' here?
+    private static final int MAXVAR = NN * NN * NN + 1; // XXX: Do we really need '+1' here?
 
     /***
      * Find a SAT4J (Dimacs) variable id (number) to represent a digit in a
@@ -249,7 +249,7 @@ public class PuzzleSolver {
      * @return The variable id.
      */
     private static int var(int x, int y, int d) {
-        return x * 9 + y * 81 + d;
+        return x * NN + y * NN * NN + d;
     }
 
     /***
@@ -260,7 +260,7 @@ public class PuzzleSolver {
      * @return The X position in range 0..8.
      */
     private static int varX(int id) {
-        return (id - 1) / 9 % 9;
+        return (id - 1) / NN % NN;
     }
 
     /***
@@ -271,7 +271,7 @@ public class PuzzleSolver {
      * @return The Y position in range 0..8.
      */
     private static int varY(int id) {
-        return (id - 1) / 81;
+        return (id - 1) / (NN * NN);
     }
 
     /***
@@ -282,6 +282,6 @@ public class PuzzleSolver {
      * @return The digit in range 1..9.
      */
     private static int varD(int id) {
-        return (id - 1) % 9 + 1;
+        return (id - 1) % NN + 1;
     }
 }
